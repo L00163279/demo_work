c274408369b914d8f75203b7a721c811b93275bf
MShafeeq <L00163279@student.lyit.ie>
2021-10-22 11:49:40 +0100

    second commit

---
+++ demo/q1.py
@@ -0,0 +1,36 @@
+# .............................
+
+# File: .py
+# Author - Muhammed Shafeeq Thottathil
+# Version:
+# Created Date:  
+# Modified Date: 
+# Description :
+# Listening : 
+
+# ...............................
+
+if __name__ == "__main__":
+    print("helo world")
+
+'''
+
+   Main method of application 
+
+
+
+   Linear programming only presented here wrt demo of lists
+
+
+
+   Parameters:
+
+    none
+
+
+
+   Returns:
+
+    none
+
+  '''
